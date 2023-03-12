e33ebffb7b17dfb05eeddf3537b266e4048a8aeb
lingtingAG <zsfLgYT@163.com>
2023-03-12 17:59:25 +0800

    删除无用文件

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/github-pages-demo1/dist/',
+  // base:'/github-pages-demo1/dist/',
   plugins: [vue()]
 })
