3d3a0fec628acbdd5e34367984b2d51cd6f3988f
lingtingAG <zsfLgYT@163.com>
2023-03-12 19:53:07 +0800

    ferature:创建欢迎界面

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,15 +1,35 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
-import {Foo} from '../views/Foo';
-import { Bar } from '../views/Bar'
+import {
+  RouteRecord,
+  RouteRecordRaw,
+  createRouter,
+  createWebHashHistory,
+} from "vue-router";
+import { Foo } from "../views/Foo";
+import { Bar } from "../views/Bar";
+import { Welcome } from "../views/Welcome";
+import { First } from "../components/welcome/First";
+import { Second } from "../components/welcome/Second";
+import { Third } from "../components/welcome/Third";
+import { Fourth } from "../components/welcome/Fourth";
 
-const routes = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
-]
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: Foo },
+  { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Welcome,
+    children: [
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Fourth },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
 
 export default router;
