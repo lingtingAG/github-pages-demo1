efa3a371fc82e534fdd0c618e3b4610d9dee60a3
lingtingAG <zsfLgYT@163.com>
2023-03-27 21:22:43 +0800

    扩展 Tags 组件，支持 classPrefix

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,5 +1,8 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './ItemList.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
 export const ItemList = defineComponent({
   props: {
     name: {
@@ -7,8 +10,22 @@ export const ItemList = defineComponent({
     }
   },
   setup: (props, context) => {
+    const refSelected = ref('本月');
     return () => (
-      <div class={s.wrapper}>list</div>
+      <MainLayout>{
+        {
+          title: () => '山竹记账',
+          icon: () => <Icon name="menu"></Icon>,
+          default: () => (
+            <Tabs v-model:selected={refSelected.value} classPrefix="lt">
+              <Tab name="本月" >list1</Tab>
+              <Tab name="上月">list2</Tab>
+              <Tab name="今年">list3</Tab>
+              <Tab name="自定义时间">list4</Tab>
+            </Tabs>
+          )
+        }
+      }</MainLayout>
     )
   }
 })
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -3,6 +3,9 @@ import s from "./Tabs.module.scss";
 
 export const Tabs = defineComponent({
   props: {
+    classPrefix: {
+      type: String as PropType<string>,
+    },
     selected: {
       type: String as PropType<string>,
     },
@@ -16,12 +19,16 @@ export const Tabs = defineComponent({
           throw new Error("<Tabs> only accepts <Tab> as Children");
         }
       }
+      const cp = props.classPrefix;
       return (
-        <div class={s.tabs}>
-          <ol class={s.tabs_nav}>
+        <div class={[s.tabs, s[cp + '_tabs']]}>
+          <ol class={[s.tabs_nav, s[cp + '_tabs_nav']]}>
             {tabs.map((item) => (
               <li
-                class={item.props?.name === props.selected ? s.selected : ""}
+                class={[
+                  item.props?.name === props.selected ? [s.selected, s[cp + 'selected']] : "",
+                  s[cp + '_tabs_nav_item']
+                ]}
                 onClick={() => context.emit('update:selected', (item.props?.name))}
               >
                 {item.props?.name}
