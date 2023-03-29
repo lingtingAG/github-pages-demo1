1eec8518c021da6e8ed946fdd2bc2feba5df4458
lingtingAG <zsfLgYT@163.com>
2023-03-29 20:51:53 +0800

    修改vant css变量

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import { App } from "./App";
 import router from "./router/routes";
 import "vant/lib/index.css";
+import "./assets/stylesheets/reset.scss";
+import "./assets/stylesheets/vars.scss";
 import "@svgstore";
 
 const app = createApp(App);
