74e3111e2e4d775242dff77b930372e76f5a45a5
lingtingAG <zsfLgYT@163.com>
2023-03-30 22:44:59 +0800

    创建 Charts 组件，添加select

--- src/shared/Form.tsx
+++ src/shared/Form.tsx
@@ -29,12 +29,15 @@ export const FormItem = defineComponent({
       type: [String, Number, Date]
     },
     type: {
-      type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode'>,
+      type: String as PropType<'text' | 'emojiSelect' | 'date' | 'validationCode' | 'select'>,
     },
     error: {
       type: String,
     },
     placeholder: String,
+    options: {
+      type: Array as PropType<Array<{ value: string, text: string }>>,
+    }
   },
   emits:['update:modelValue'],
   setup: (props, context) => {
@@ -77,7 +80,14 @@ export const FormItem = defineComponent({
                   }}
                   onCancel={() => refDateVisible.value = false} />
               </Popup>
-            </>
+          </>
+        case 'select':
+          return <select class={[s.formItem, s.select]} value={props.modelValue}
+            onChange={(e: any) => context.emit('update:modelValue', e.target.value)}>
+            {props.options?.map(option =>
+              <option value={option.value}>{option.text}</option>
+            )}
+          </select>
         case undefined:
           return context.slots.default?.()
       }
